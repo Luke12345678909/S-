05ca4e9ed31dc0d62d2d1c500cdde6937f070f99
Luke12345678909 <31909239+Luke12345678909@users.noreply.github.com>
2018-01-15 13:19:36 -0800

    Create S#.js

---
+++ src/S#.js
@@ -0,0 +1,23 @@
+var System=function(w,h){
+this.start=function(){
+for(var i=0;i<w;i++){
+for(var j=0;j<h;j++){
+this.pixels.push([]);
+this.pixels[i].push(color(255,255,255));
+}
+}
+};
+this.point=function(x,y,col){
+if(x<w&&y<w&&x>0&&y>0){
+this.pixels[x][y]=col;
+}
+};
+this.updateScreen=function(){
+for(var i=0;i<w;i++){
+for(var j=0;j<h;j++){
+stroke(this.pixels[i][j]);
+point(i,j);
+}
+}
+}
+};
